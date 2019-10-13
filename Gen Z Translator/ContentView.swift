99b916167365cf8994dd96e8aa3b0a2b3d903fcb
aercm <38596286+aercm@users.noreply.github.com>
2019-10-12 18:06:23 -0700

    Add files via upload

---
+++ Gen Z Translator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gen Z Translator
+//
+//  Created by Aerin Malana on 10/12/19.
+//  Copyright © 2019 Aerin Malana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
